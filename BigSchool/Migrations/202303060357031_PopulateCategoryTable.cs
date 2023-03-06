1090d1ed2a71d9239dd71542e0e1749052c3cc9c
MaggieCohans <maggiecohans274@gmail.com>
2023-03-06 11:21:19 +0700

    Add project files.

---
+++ BigSchool/Migrations/202303060357031_PopulateCategoryTable.cs
@@ -0,0 +1,19 @@
+﻿namespace BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateCategoryTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("Insert into Categories(ID,NAME) values (1,'Develop')");
+            Sql("Insert into Categories(ID,NAME) values (2,'Dev Front-end')");
+            Sql("Insert into Categories(ID,NAME) values (3,'Dev Back-end')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ BigSchool/Models/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Category> Categories{ get; set; } //Nhớ thêm cái này vô trước khi add-migration 'createTable' thì mới ra trong database
+        public DbSet<Course> Courses{ get; set; }
+public ApplicationDbContext()
+            : base("MaggieDb", throwIfV1Schema: false) //nhớ đổi tên trong base
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
