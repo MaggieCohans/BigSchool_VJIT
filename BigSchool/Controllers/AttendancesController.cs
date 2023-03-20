cc88f1b4927a21269e1f9bdad8f38635a6718735
MaggieCohans <maggiecohans274@gmail.com>
2023-03-20 10:49:45 +0700

    Lab 20/03

---
+++ BigSchool/Controllers/AttendancesController.cs
@@ -0,0 +1,25 @@
+﻿using BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    private ApplicationDbContext _dbContext;
+    public class AttendancesController : ApiController
+    {
+        _dbContext=new ApplicationDbContext();
+
+    }
+    [HttpPost]
+    public IHttpActionResult Attend([FromBody] int courseId)
+    {
+        var attendance=new Attendance(
+
+            )
+    }
+}
